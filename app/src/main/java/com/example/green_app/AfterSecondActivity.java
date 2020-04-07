8297ddc6c154947ee189ef9062e35b4877c8fab3
GeorgeSUe <oopsu425@gmail.com>
2020-04-08 03:42:00 +0800

    UX Design 7: Android UIs

---
+++ app/src/main/java/com/example/green_app/AfterSecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.green_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AfterSecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_second_activity);
+    }
+}
